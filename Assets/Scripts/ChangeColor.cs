e0fc610df7d2b5beeac22c77106e83e256893728
Azrielx86 <edgar1569@outlook.com>
2025-03-04 23:35:59 -0600

    Added colors array

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -6,33 +6,34 @@ using UnityEngine;
 public class ChangeColor : MonoBehaviour
 {
     public GameObject model;
-    public Color color;
+    public List<Color> colors;
     public Material material;
 
     private Color _defaultModelColor;
     private Color _defaultMaterialColor;
     private bool _hasColorChanged;
+    private int _colorIndex = 0;
 
     void Start()
     {
         _defaultModelColor = model.GetComponent<Renderer>().material.color;
         _defaultMaterialColor = material.color;
-        _hasColorChanged = false;
+        _colorIndex = -1;
     }
 
     public void DoChangeColor()
     {
-        if (_hasColorChanged)
+        _colorIndex++;
+        if (_colorIndex >= colors.Count)
         {
             model.GetComponent<Renderer>().material.color = _defaultModelColor;
             material.color = _defaultMaterialColor;
-            _hasColorChanged = false;
+            _colorIndex = -1;
         }
         else
         {
-            model.GetComponent<Renderer>().material.color = color;
-            material.color = color;
-            _hasColorChanged = true;
+            model.GetComponent<Renderer>().material.color = colors[_colorIndex];
+            material.color = colors[_colorIndex];
         }
     }
 }
